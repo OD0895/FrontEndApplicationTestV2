e1c2f5f16bc5023efe0fef897bf9ec21caf25d9f
Osvaldo Amador <34145409+OD0895@users.noreply.github.com>
2022-03-11 12:17:25 -0600

    Add variable urlApi ENV PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  appIdWeather: '212d2fc15438bc7c52d1d7d88dcc1cde'
+  appIdWeather: '212d2fc15438bc7c52d1d7d88dcc1cde',
+  urlApi: 'http://localhost:3002/api'
 };
